8136612a59bb908ca0a20d6e26bd477c78b498bb
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-11-05 17:06:04 +0100

    remove unused logo.svg file

---
+++ 2.0.0-SNAPSHOT/search/classes_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['unexpectedstatuswordexception_0',['UnexpectedStatusWordException',['../classkeypop_1_1card_1_1_unexpected_status_word_exception.html',1,'keypop::card']]]
+];
---
+++ 2.0.0-SNAPSHOT/search/files_1.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['cardapiproperties_2ehpp_0',['CardApiProperties.hpp',['../_card_api_properties_8hpp.html',1,'']]],
+  ['cardbrokencommunicationexception_2ehpp_1',['CardBrokenCommunicationException.hpp',['../_card_broken_communication_exception_8hpp.html',1,'']]],
+  ['cardrequestspi_2ehpp_2',['CardRequestSpi.hpp',['../_card_request_spi_8hpp.html',1,'']]],
+  ['cardresponseapi_2ehpp_3',['CardResponseApi.hpp',['../_card_response_api_8hpp.html',1,'']]],
+  ['cardselectionextensionspi_2ehpp_4',['CardSelectionExtensionSpi.hpp',['../_card_selection_extension_spi_8hpp.html',1,'']]],
+  ['cardselectionrequestspi_2ehpp_5',['CardSelectionRequestSpi.hpp',['../_card_selection_request_spi_8hpp.html',1,'']]],
+  ['cardselectionresponse_2ehpp_6',['CardSelectionResponse.hpp',['../_card_selection_response_8hpp.html',1,'']]],
+  ['channelcontrol_2ehpp_7',['ChannelControl.hpp',['../_channel_control_8hpp.html',1,'']]]
+];
